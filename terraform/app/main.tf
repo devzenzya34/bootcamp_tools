dff97c01d617307328f9c707fc0edd883d011df8
DevZenzya34 <devzenzya34@gmail.com>
2025-02-21 13:26:51 +0100

    Fix: Error after terraform validate

--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -13,34 +13,34 @@ data "aws_ami" "ubuntu_ami" {
 
 
 module "security_group" {
-  source = "../modules/security_group"
-  sg_name = var.security_group_name
-  sg_tag = var.security_group_tag
+  source              = "../modules/security_group"
+  security_group_name = var.security_group_name
+  security_group_tag  = var.security_group_tag
 }
 
 module "ec2" {
-  source = "../modules/ec2"
-  instance_tag = var.instance_tag
-  user = var.user
-  instance_type = var.instance_type
-  key_name = var.key_name
+  source              = "../modules/ec2"
+  instance_tag        = var.instance_tag
+  user                = var.user
+  instance_type       = var.instance_type
+  key_name            = var.key_name
   security_group_name = module.security_group.security_group_id
 }
 
 module "eip" {
-  source = "../modules/eip"
+  source  = "../modules/eip"
   eip_tag = var.eip_tag
 }
 
 module "ebs" {
-  source = "../modules/ebs"
-  ebs_tag = var.ebs_tag
+  source   = "../modules/ebs"
+  ebs_tag  = var.ebs_tag
   ebs_size = var.ebs_size
   ebs_zone = var.ebs_zone
 }
 
 resource "aws_eip_association" "eip_association" {
-  instance_id = module.ec2.ec2_id
+  instance_id   = module.ec2.ec2_id
   allocation_id = module.eip.eip_id
 }
 
--- terraform/app/provider.tf
+++ terraform/app/provider.tf
@@ -1,13 +1,13 @@
 terraform {
-    required_version = "1.10.5"
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "4.61.0"
-      }
+  required_version = "1.10.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.61.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
