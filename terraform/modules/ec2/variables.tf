db51058f57281cccfa3efc4cba54aa95fea33c94
DevZenzya34 <devzenzya34@gmail.com>
2025-02-21 10:25:27 +0100

    Feat: write ec2 module

---
+++ terraform/modules/ec2/variables.tf
@@ -0,0 +1,31 @@
+variable "instance_type" {
+  type = string
+  default = "t2.medium"
+  description = "The type for EC2 instance to start"
+}
+
+variable "key_name" {
+  type = string
+  default = "mini-terraform-key"
+  description = "The name of the key pair to use for SSH access to the instance"
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    Name = "ec2"
+  }
+  description = "The tags to apply to the EC2 instance"
+}
+
+variable "security_group_name" {
+  type        = string
+  default     = "security_group"
+  description = "Name of the security group"
+}
+
+variable "user" {
+  type        = string
+  default     = "ubuntu"
+  description = "The user to connect to the EC2 instance"
+}
